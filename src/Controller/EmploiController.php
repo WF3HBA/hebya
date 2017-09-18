97f9a41aa4da64e8fc49f7000c580dea75e9a23d
idsa1992 <said.hamrouni@hotmail.fr>
2017-09-18 19:04:13 +0200

    modif emploi

--- src/Controller/EmploiController.php
+++ src/Controller/EmploiController.php
@@ -8,9 +8,13 @@ namespace Controller;
 class EmploiController extends ControllerAbstract {
    
     public function emploiAction() {
+        $countries = $this->app['country.repository']->findAll();
       
         return $this->render(
-                'emploi/emploi.html.twig'
+                'emploi/emploi.html.twig',
+                [
+                    'countries' => $countries
+                ]
         );
     }
 }
