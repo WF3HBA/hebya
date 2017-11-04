6ea0516486ae82bd156d48249033eec369fd127d
sdurrbach <sebastien.durrbach@gmail.com>
2017-11-04 16:53:23 +0100

    seb

--- web/js/public/clients.js
+++ web/js/public/clients.js
@@ -5,6 +5,31 @@ $(function(){
         speed: 0.3
     });
     
+    $(document).on("click", ".button-detail", function () {
+    
+    var $client = $(this).closest('.client-box');
+    var name = $client.find('.client-name').html();
+    var content = $client.find('.client-description').html();
+    var picture = $client.find('img').attr('src');
+    
+    console.log(name);
+
+    $('#client-overlay h2').html(name);
+    $('#client-overlay img').attr('src', picture);
+    $('#client-detail').html(content);
+    
+    $('body').toggleClass('noscroll');
+    $('#client-overlay').fadeIn(600);
+    
+  });
+  
+  $('#close-overlay').click(function(){
+    
+    $('#client-overlay').animate({scrollTop:0},200);
+    $('#client-overlay').fadeOut();
+    $('body').toggleClass('noscroll');
+  });
+    
 });
 
 
